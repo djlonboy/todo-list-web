d7a59b901eebb9829b71f675f144a86c50eb32fb
djlonboy <djlonboy@gmail.com>
2019-11-19 12:19:50 +0000

    Added failign test for reding items in todo list

--- spec/units/todo_list_spec.rb
+++ spec/units/todo_list_spec.rb
@@ -4,10 +4,14 @@ describe ToDoList do
  it 'can add something into the todolist' do
    todolist = ToDoList.new
    todolist.add("testitem")
-   expect(todolist.items).to include("testitem")
+   expect(todolist.items).to include "testitem"
  end
 
  it 'read all the items in the todo list' do
    todolist = ToDoList.new
+   todolist.add("testitem1")
+   todolist.add("testitem2")
+   expect(todolist.list).to eq "1. testitem1\n2. testitem2"
+
  end
 end
