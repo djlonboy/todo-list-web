0f39885d827bd3934a7d9ced4731fd1c9c2bc114
djlonboy <djlonboy@gmail.com>
2019-11-20 10:59:27 +0000

    Created failing feature test for marking item complete

--- spec/features/todolist_spec.rb
+++ spec/features/todolist_spec.rb
@@ -1,12 +1,22 @@
 require "capybara/rspec"
 
-feature "Add Item" do
-
-  scenario "I should be able to add a todo item" do
+feature "Add Item:" do
+  scenario "Should be able to add a todo item" do
     visit '/add-item'
     fill_in('item', :with => 'Buy stamps')
     click_button('submit')
     expect(page).to have_content 'Buy stamps'
+  end
+end
 
+  feature "Complete item:" do
+    scenario "Should be able to mark item complete" do
+      visit '/add-item'
+      fill_in('item', :with => 'Buy stamps')
+      click_button('submit')
+      visit "/mark-complete"
+      fill_in('itemnumber', :with => '1')
+      click_button('submit')
+      expect(page).to have_content 'Buy stamps - Complete'
   end
 end
